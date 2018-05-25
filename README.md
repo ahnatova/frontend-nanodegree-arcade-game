fcd2522f37839ffcab3728118656750bc799b6e5
Alena-Hnatova <a.hnatova@seznam.cz>
2018-05-25 12:59:27 +0200

    Added collision check, updated readme.md

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-frontend-nanodegree-arcade-game
-===============================
+How to start the game:
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+1. Open the index.html file
+2. Use the arrow keys to move the character, that is located in the middle of the bottom row
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+How to win the game:
+Your goal is to reach the water (top row), whey you do so, the game ends and an message appears on your screen saying that you've won.
+
+However, on your way there, you have to avoid those nasty bugs. If you run into a bug, the game resets your character to the initial position.
--- js/app.js
+++ js/app.js
@@ -1,28 +1,24 @@
 // Enemies our player must avoid
 var Enemy = function(x,y,speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
     this.x += this.speed*dt;
-    let round = 0;
     if(this.x>=505) {
         this.x = 0;
         this.speed = this.speed + (Math.floor((Math.random() * 10) + 1));
     } 
+
+    //collision
+   if(this.x < player.x + 55 && this.x + 55 > player.x && this.y < player.y + 30 && this.y + 30 > player.y) {
+       player.resetPosition();
+   }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -30,26 +26,23 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 const Player = function(x, y) {
     this.x = x;
-    this.y = y;
+    this.y = y;    
     this.sprite = 'images/char-boy.png';
 }
 
 Player.prototype.update = function(dt) { 
-    if (player.y < 40) { //resets player's position after reaching the water
-        this.x = 200;
-        this.y = 400;
-    } else if (player.x > 400) { //prohibits movement out of the canvas x axis
+    if (this.y < 40) { //resets player's position after reaching the water
+        this.resetPosition();
+        alert("You've won!");
+    } else if (this.x > 400) { //prohibits movement out of the canvas x axis
         this.x = 400;
-    } else if (player.x < 0) { //prohibits movement out of the canvas y axis
+    } else if (this.x < 0) { 
         this.x = 0;
-    } else if (player.y > 400) {
+    } else if (this.y > 400) { //prohibits movement out of the canvas y axis
         this.y = 400;
-    }
+    } 
 };
 
 Player.prototype.render = function() {
@@ -59,30 +52,26 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function(key) {
     if (key === 'left') {
         this.x = this.x - 100;
-        (console.log(this.x));
     } else if (key === 'up') {
         this.y = this.y - 90;
-        (console.log(this.y));
     } else if (key === 'right') {
         this.x = this.x + 100;
-        (console.log(this.x));
     } else if (key === 'down') {
         this.y = this.y + 90;
-        (console.log(this.y));
     }
 };
 
-// Now instantiate your objects.
-const firstEnemy = new Enemy(0,60,50);
-const secondEnemy = new Enemy(0,140,100);
+Player.prototype.resetPosition = function() {
+    this.x = 200;
+    this.y = 400;
+};
+
+const firstEnemy = new Enemy(0,40,50);
+const secondEnemy = new Enemy(0,130,100);
 const thirdEnemy = new Enemy(0,220,30);
 
 const player = new Player(200,400);
-// Place all enemy objects in an array called allEnemies
-const allEnemies = [firstEnemy,secondEnemy,thirdEnemy];
-// Place the player object in a variable called player
-
-
+const allEnemies = [firstEnemy, secondEnemy, thirdEnemy];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
